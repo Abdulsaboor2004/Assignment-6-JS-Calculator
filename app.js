470587444881fbdbd8acbf9289616815553fa165
Abdulsaboor2004 <abdulsaboor.17.4.2004@gmail.com>
2023-05-21 11:00:02 +0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,23 @@
+let screen = document.getElementById('screen')
+// screen.addEventListener("keyup", function () {
+
+// })
+let buttons = document.querySelectorAll('.buttonBox button')
+let string = "";
+
+Array.from(buttons).forEach((button) => {
+    button.addEventListener('click', function (e) {
+        if (e.target.innerHTML === '=') {
+            string = eval(string);
+            screen.value = string;
+        } else if (e.target.innerHTML === 'C') {
+            string = "";
+            screen.value = "0.00";
+        } else {
+            string += e.target.innerHTML;
+            screen.value = string;
+        }
+    });
+});
+
+
